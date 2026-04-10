00ee044a93825b82ddc8f025cb24438de0900815
Caio <caiomraphaelli@gmail.com>
2026-04-10 09:36:26 -0300

    aula 10/04/2026

---
+++ Parte3/ClassesImportantes/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ClassesImportantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnTimeSpam = new System.Windows.Forms.Button();
+            this.btnDateTime = new System.Windows.Forms.Button();
+            this.btnEnvironment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnMessageBox.Location = new System.Drawing.Point(12, 410);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(143, 54);
+            this.btnMessageBox.TabIndex = 0;
+            this.btnMessageBox.Text = "MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = false;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 9);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(835, 350);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnRandom.Location = new System.Drawing.Point(191, 410);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(143, 54);
+            this.btnRandom.TabIndex = 2;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = false;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnTimeSpam
+            // 
+            this.btnTimeSpam.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnTimeSpam.Location = new System.Drawing.Point(365, 410);
+            this.btnTimeSpam.Name = "btnTimeSpam";
+            this.btnTimeSpam.Size = new System.Drawing.Size(143, 54);
+            this.btnTimeSpam.TabIndex = 3;
+            this.btnTimeSpam.Text = "TimeSpam";
+            this.btnTimeSpam.UseVisualStyleBackColor = false;
+            this.btnTimeSpam.Click += new System.EventHandler(this.btnTimeSpam_Click);
+            // 
+            // btnDateTime
+            // 
+            this.btnDateTime.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnDateTime.Location = new System.Drawing.Point(526, 410);
+            this.btnDateTime.Name = "btnDateTime";
+            this.btnDateTime.Size = new System.Drawing.Size(143, 54);
+            this.btnDateTime.TabIndex = 4;
+            this.btnDateTime.Text = "DateTime";
+            this.btnDateTime.UseVisualStyleBackColor = false;
+            this.btnDateTime.Click += new System.EventHandler(this.btnDateTime_Click);
+            // 
+            // btnEnvironment
+            // 
+            this.btnEnvironment.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnEnvironment.Location = new System.Drawing.Point(687, 410);
+            this.btnEnvironment.Name = "btnEnvironment";
+            this.btnEnvironment.Size = new System.Drawing.Size(143, 54);
+            this.btnEnvironment.TabIndex = 5;
+            this.btnEnvironment.Text = "Environment";
+            this.btnEnvironment.UseVisualStyleBackColor = false;
+            this.btnEnvironment.Click += new System.EventHandler(this.btnEnvironment_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 476);
+            this.Controls.Add(this.btnEnvironment);
+            this.Controls.Add(this.btnDateTime);
+            this.Controls.Add(this.btnTimeSpam);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnMessageBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnTimeSpam;
+        private System.Windows.Forms.Button btnDateTime;
+        private System.Windows.Forms.Button btnEnvironment;
+    }
+}
+
---
+++ Parte3/ClassesImportantes/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassesImportantes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+            //MessageBox é uma classe do namespace System.Windows.Forms que exibe uma caixa de diálogo com uma mensagem para o usuário.
+            //Tem 20 overloads, o mais simples é a string
+
+            //Aqui setamos os botoes que queremos que apareçam, bem como o Título
+            DialogResult res =  MessageBox.Show("Mensagem", "Título", MessageBoxButtons.YesNoCancel);
+
+            if (res == DialogResult.Yes)
+            {
+                lblResultado.Text = "Você clicou em Sim";
+            }
+
+            MessageBox.Show("Mensagem", "Título", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button3);
+   
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            //O numero nao e aleatorio de verdade, ele é gerado a partir de um algoritmo, por isso é chamado de pseudo-aleatório
+
+            //Aqui inicializamos o gerador de números aleatórios com uma semente, que é o valor usado para iniciar o algoritmo de geração de números aleatórios.
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            random.Next(10, 100); //Gera um número aleatório entre 10 e 99 (o limite superior é exclusivo)
+            int valor = random.Next();
+
+            lblResultado.Text = $"Número aleatório: {valor}";
+        }
+
+        private void btnTimeSpam_Click(object sender, EventArgs e)
+        {
+            //TimeSpan é uma estrutura representa um intervalo de tempo. Ele pode ser usado para medir o tempo decorrido entre dois eventos ou para representar uma duração específica.
+            //Ticks são a menor unidade de tempo representada pelo TimeSpan, onde 1 tick é igual a 100 nanosegundos.
+
+            //Varios metodos, da classe, objetos instaciaods e propriedades.
+
+            TimeSpan.FromDays(12.6); //Cria um TimeSpan de 12,6 dias
+
+            TimeSpan tempo = new TimeSpan(1, 30, 45 ); //Cria um TimeSpan de 1 hora, 30 minutos e 45 segundos
+            tempo.Hours.ToString(); //Retorna a parte de horas do TimeSpan como string
+
+            lblResultado.Text = $"TimeSpan: {TimeSpan.FromSeconds(90)}"; //Cria um TimeSpan de 90 segundos
+        }
+
+        private void btnDateTime_Click(object sender, EventArgs e)
+        {
+            //DateTime é uma estrutura que representa um ponto específico no tempo, incluindo data e hora. Ele pode ser usado para armazenar e manipular informações de data e hora em um programa.
+            lblResultado.Text = $"Data e Hora atual: {DateTime.Now}"; //Retorna a data e hora atual do sistema
+        }
+
+        private void btnEnvironment_Click(object sender, EventArgs e)
+        {
+            //Classe Environment fornece informações sobre o ambiente de execução do aplicativo, como variáveis de ambiente, diretórios do sistema, informações de processador, etc.
+            
+            lblResultado.Text = $"Nome do computador: {Environment.MachineName}\n" +
+                                $"Sistema Operacional: {Environment.OSVersion}\n" +
+                                $"Número de processadores: {Environment.ProcessorCount}\n" +
+                                $"Diretório atual: {Environment.CurrentDirectory}";
+
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //Retorna o caminho do diretório "Meus Documentos" do usuário atual
+            Environment.GetEnvironmentVariable("PATH"); //Retorna o valor da variável de ambiente "PATH"
+
+            //Tambem tem a classe Aplication , que fornece informações sobre o aplicativo em execução, como o caminho do executável, a versão do aplicativo, etc.
+        }
+    }
+}
